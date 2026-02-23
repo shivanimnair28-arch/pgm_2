75dac53a992b57c8bab9ee29888d046ed2950696
shivanimnair28-arch <shivanimnair28@gmail.com>
2026-02-23 13:00:32 +0530

    Update pgm2.java

--- pgm2.java
+++ pgm2.java
@@ -1,5 +1,5 @@
 public class Program2 {
  public static void main(String[] args) {
- System.out.println("This is Program 2 - Maven Jenkins Demo");
+ System.out.println("This is Program 2 - Maven Jenkins Demo with some changes");
  }
 }
